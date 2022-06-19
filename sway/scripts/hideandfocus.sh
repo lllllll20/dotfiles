509d6de5cd82e64032461193daebf72c04e9f94d
lllllll20 <lllllll@mailbox.org>
2022-06-19 21:38:53 +0100

    minor changes

--- sway/scripts/hideandfocus.sh
+++ sway/scripts/hideandfocus.sh
@@ -5,8 +5,8 @@ cur_focus="$(swaymsg -t get_tree | jq -r '.. | select(.type?) | select(.focused=
 if [ "$cur_focus" == "floatterm" ] || [ "$cur_focus" == "music" ] || [ "$cur_focus" == "files" ]; then
     swaymsg scratchpad show
 fi
-if [[ "$1" == "keepassxc" ]]; then
-    wlrctl window focus "org.keepassxc.KeePassXC" || swaymsg exec "$1"
+if [[ "$1" == "secrets" ]]; then
+    wlrctl window focus "org.gnome.World.Secrets" || swaymsg exec "$1"
 
 elif [[ "$1" == "footws2" ]]; then
     wlrctl window focus "footws2" || swaymsg exec "foot -a footws2"

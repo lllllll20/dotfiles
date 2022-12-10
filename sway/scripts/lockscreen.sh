55a45677b47ca8a6d3674f488e06aec6dd0d2432
lllllll20 <lllllll@mailbox.org>
2022-12-10 10:31:34 +0000

    minor changes

--- sway/scripts/lockscreen.sh
+++ sway/scripts/lockscreen.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/bash
 
-export XDG_RUNTIME_DIR=/run/user/$(id -u)
-export WAYLAND_DISPLAY=wayland-1
+WAYLAND_DISPLAY=wayland-1
+XDG_RUNTIME_DIR=/run/user/$(id -u)
+export XDG_RUNTIME_DIR
+export WAYLAND_DISPLAY
 
+playerctl pause
 swaylock -u -i ~/computing/wallpapers/"245.jpg"

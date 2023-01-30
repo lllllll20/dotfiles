0c3f4e8c17707ed37dc3e5d10ac446cf214e2ccb
lllllll20 <lllllll@mailbox.org>
2023-01-30 06:42:45 +0000

    minor changes

--- emacs/init.el
+++ emacs/init.el
@@ -69,7 +69,8 @@
 
 (global-set-key "\C-cc" 'org-capture)
 
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+					;
+;(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
@@ -146,6 +147,9 @@
 
 ;; Avoid lock files
 (setq create-lockfiles nil)
+
+;; Copy between open dired-buffers
+(setq dired-dwim-target t)
 
 (use-package projectile
   :diminish projectile-mode
@@ -262,8 +266,13 @@
 (global-set-key (kbd "<C-M-left>") 'previous-buffer)
 (global-set-key (kbd "<C-M-right>") 'next-buffer)
 (global-set-key (kbd "C-c n") 'me/counsel-notes)
+(global-set-key (kbd "C-.") 'other-window)
 
 
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
 
 
@@ -274,7 +283,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons-ivy-rich all-the-icons rainbow-delimiters ivy-rich helpful which-key doom-modeline use-package shrink-path doom-themes dired-hide-dotfiles counsel-projectile compat)))
+   '(magit all-the-icons-ivy-rich all-the-icons rainbow-delimiters ivy-rich helpful which-key doom-modeline use-package shrink-path doom-themes dired-hide-dotfiles counsel-projectile compat)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

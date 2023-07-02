10e63fde88323bfd2606c2016a31de1ab5c6a8e4
lllllll20 <lllllll@mailbox.org>
2023-07-02 08:47:20 +0100

    changes

--- emacs/init.el
+++ emacs/init.el
@@ -291,7 +291,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit all-the-icons-ivy-rich all-the-icons rainbow-delimiters helpful which-key doom-modeline use-package shrink-path doom-themes dired-hide-dotfiles compat)))
+   '(lsp-ui lsp-mode magit all-the-icons-ivy-rich all-the-icons
+	    rainbow-delimiters helpful which-key doom-modeline
+	    use-package shrink-path doom-themes dired-hide-dotfiles
+	    compat)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
